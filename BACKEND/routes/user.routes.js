7034842e362c6b515bc5dc606357d5dab44980cb
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-03-29 17:42:39 +0530

    fix some bug

--- BACKEND/routes/user.routes.js
+++ BACKEND/routes/user.routes.js
@@ -1,21 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
+import { registerCustomer } from "../controllers/Customer.controller.js";
+// import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/regidter").post(
-    upload.fields([ // to check for images and check for avatar
-        {
-            name: "avatar", 
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser)
+router.route("/Sign-up").post(registerCustomer)
 
 
 
